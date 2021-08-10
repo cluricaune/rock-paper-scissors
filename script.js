1fd1842defbabfd99cc99ab7d921098f945d590e
cluricaune <dunn.jake90@gmail.com>
2021-08-10 17:02:45 -0400

    .

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Hello, World!");
 
+
 function computerPlay(){
     let choices = ["Rock", "Paper", "Scissors"];
     let randomChoice = Math.floor(Math.random() * choices.length);
@@ -14,36 +15,51 @@ function computerPlay(){
     }
 }
 
-let playerChoice = prompt("Rock, Paper, or Scissors?", "Rock").toUpperCase();
-let computerChoice = computerPlay().toUpperCase();
+
 
 function oneRound(){
-    if (playerChoice === "ROCK") {
-        if (computerChoice === "ROCK") {
-            return "Tie! Go again.";
-        } else if (computerChoice === "PAPER") {
+    let playerChoice = prompt("Rock, Paper, or Scissors?", "Rock").toUpperCase();
+    let computerChoice = computerPlay().toUpperCase();
+    let playerScore = 0;
+    let computerScore = 0;
+    game(playerChoice, playerScore, computerChoice, computerScore)
+    if (playerChoice === computerChoice) {
+        return "Tie! Go again."
+    } else if (playerChoice === "ROCK") {
+        if (computerChoice === "PAPER") {
+            computerScore += 1;
             return "Computer wins! Paper beats rock.";
         } else if (computerChoice === "SCISSORS") {
-            return "Player wins! Rock beats scissors."
+            playerScore += 1;
+            return "Player wins! Rock beats scissors.";
         }
     } else if (playerChoice === "PAPER") {
         if (computerChoice === "ROCK") {
+            playerScore += 1;
             return "Player wins! Paper beats rock.";
-        } else if (computerChoice === "PAPER") {
-            return "Tie! Go again.";
         } else if (computerChoice === "SCISSORS") {
+            computerScore += 1;
             return "Computer wins! Scissors beat paper.";
         }
     } else if (playerChoice === "SCISSORS") {
         if (computerChoice === "ROCK") {
+            computerScore += 1;
             return "Computer wins! Rock beats scissors.";
         } else if (computerChoice === "PAPER") {
+            playerScore += 1;
             return "Player wins! Scissors beat paper.";
-        } else if (computerChoice === "SCISSORS") {
-            return "Tie! Go again.";
         }
-    } else {
-        return "Uh oh, did you pick rock, paper or scissors? Check spelling."
     }
+    else {
+        return "Uh oh, did you pick rock, paper or scissors? Check spelling.";
+    }
+    
 }
-alert(oneRound() + `\nPlayer: ${playerChoice}\nComputer: ${computerChoice}`)
+function game() {
+    alert(oneRound() + `\nPlayer: ${playerChoice} (${playerScore})\nComputer: ${computerChoice} (${computerScore})`);
+}
+game();
+
+
+
+

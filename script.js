f3fddd9f3bc685e2eab9307f06311a9fb2902002
cluricaune <dunn.jake90@gmail.com>
2021-08-10 21:08:22 -0400

    .

--- script.js
+++ script.js
@@ -58,23 +58,35 @@ function oneRound(){
     }
 }
 
-let game1 = oneRound();
+function game() {
+    let game1 = oneRound();
 
-alert(`${game1.rs}\nPlayer: ${game1.pc} (${game1.ps})\nComputer: ${game1.cc} (${game1.cs})`);
+    alert(`${game1.rs}\nPlayer: ${game1.pc} (${game1.ps})\nComputer: ${game1.cc} (${game1.cs})`);
+    
+    let game2 = oneRound();
+    
+    alert(`${game2.rs}\nPlayer: ${game2.pc} (${game2.ps})\nComputer: ${game2.cc} (${game2.cs})`);
+    
+    let game3 = oneRound();
+    
+    alert(`${game3.rs}\nPlayer: ${game3.pc} (${game3.ps})\nComputer: ${game3.cc} (${game3.cs})`);
+    
+    let game4 = oneRound();
+    
+    alert(`${game4.rs}\nPlayer: ${game4.pc} (${game4.ps})\nComputer: ${game4.cc} (${game4.cs})`);
+    
+    let game5 = oneRound();
+    
+    alert(`${game5.rs}\nPlayer: ${game5.pc} (${game5.ps})\nComputer: ${game5.cc} (${game5.cs})`);
 
-let game2 = oneRound();
+    if (game5.ps === game5.cs) {
+        return "It's a draw! Enough for now.";
+    } else if (game5.ps > game5.cs) {
+        return "You win! Computer is defeated."
+    } else if (game5.ps < game5.cs) {
+        return "You lose! The computer takes this one."
+    }
+}
+let final = game();
 
-alert(`${game2.rs}\nPlayer: ${game2.pc} (${game2.ps})\nComputer: ${game2.cc} (${game2.cs})`);
-
-let game3 = oneRound();
-
-alert(`${game3.rs}\nPlayer: ${game3.pc} (${game3.ps})\nComputer: ${game3.cc} (${game3.cs})`);
-
-let game4 = oneRound();
-
-alert(`${game4.rs}\nPlayer: ${game4.pc} (${game4.ps})\nComputer: ${game4.cc} (${game4.cs})`);
-
-let game5 = oneRound();
-
-alert(`${game5.rs}\nPlayer: ${game5.pc} (${game5.ps})\nComputer: ${game5.cc} (${game5.cs})`);
-
+alert(final);

5e9ca97d89ed628c76eab00e9e3ddd9a222b9b77
cluricaune <dunn.jake90@gmail.com>
2021-10-08 16:04:23 -0400

    begin work on UI

--- script.js
+++ script.js
@@ -1,53 +1,40 @@
 function computerPlay(){
-    let choices = ["Rock", "Paper", "Scissors"];
-    let randomChoice = Math.floor(Math.random() * choices.length);
-    if (randomChoice === 0) {
-        return "Rock";
-    } else if (randomChoice === 1) {
-        return "Paper";
-    } else if (randomChoice === 2) {
-        return "Scissors";
-    } else {
-        return "Error in computerPlay()"
-    }
+    let randomChoice = Math.floor(Math.random() * 3);
+    return randomChoice;
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
 function oneRound(){
-    let playerChoice = prompt("Rock, Paper, or Scissors?", "Rock").toUpperCase();
-    let computerChoice = computerPlay().toUpperCase();
+    let computerChoice = computerPlay();
     let result;
     if (playerChoice === computerChoice) {
         result = "Tie! Go again."
-    } else if (playerChoice === "ROCK") {
-        if (computerChoice === "PAPER") {
+    } else if (playerChoice === 0) {
+        if (computerChoice === 1) {
             computerScore += 1;
             result = "Computer wins! Paper beats rock.";
-        } else if (computerChoice === "SCISSORS") {
+        } else if (computerChoice === 2) {
             playerScore += 1;
             result = "Player wins! Rock beats scissors.";
         }
-    } else if (playerChoice === "PAPER") {
-        if (computerChoice === "ROCK") {
+    } else if (playerChoice === 1) {
+        if (computerChoice === 0) {
             playerScore += 1;
             result = "Player wins! Paper beats rock.";
-        } else if (computerChoice === "SCISSORS") {
+        } else if (computerChoice === 2) {
             computerScore += 1;
             result = "Computer wins! Scissors beat paper.";
         }
-    } else if (playerChoice === "SCISSORS") {
-        if (computerChoice === "ROCK") {
+    } else if (playerChoice === 2) {
+        if (computerChoice === 0) {
             computerScore += 1;
             result = "Computer wins! Rock beats scissors.";
-        } else if (computerChoice === "PAPER") {
+        } else if (computerChoice === 1) {
             playerScore += 1;
             result = "Player wins! Scissors beat paper.";
         }
     }
     else {
-        result = "Uh oh, did you pick rock, paper or scissors? Check spelling.";
+        result = "Something is wrong";
     }
     return {
         pc: playerChoice,
@@ -58,35 +45,35 @@ function oneRound(){
     }
 }
 
-function game() {
-    let game1 = oneRound();
 
-    alert(`${game1.rs}\nPlayer: ${game1.pc} (${game1.ps})\nComputer: ${game1.cc} (${game1.cs})`);
-    
-    let game2 = oneRound();
-    
-    alert(`${game2.rs}\nPlayer: ${game2.pc} (${game2.ps})\nComputer: ${game2.cc} (${game2.cs})`);
-    
-    let game3 = oneRound();
-    
-    alert(`${game3.rs}\nPlayer: ${game3.pc} (${game3.ps})\nComputer: ${game3.cc} (${game3.cs})`);
-    
-    let game4 = oneRound();
-    
-    alert(`${game4.rs}\nPlayer: ${game4.pc} (${game4.ps})\nComputer: ${game4.cc} (${game4.cs})`);
-    
-    let game5 = oneRound();
-    
-    alert(`${game5.rs}\nPlayer: ${game5.pc} (${game5.ps})\nComputer: ${game5.cc} (${game5.cs})`);
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice;
 
-    if (game5.ps === game5.cs) {
-        return "It's a draw! Enough for now.";
-    } else if (game5.ps > game5.cs) {
-        return "You win! Computer is defeated."
-    } else if (game5.ps < game5.cs) {
-        return "You lose! The computer takes this one."
-    }
-}
-let final = game();
+const rock = document.querySelector('#rockshape');
+const paper = document.querySelector('#papershape');
+const scissors = document.querySelector('#scissorsshape');
 
-alert(final);
+rock.addEventListener('click', () => {
+  playerChoice = 0;
+  let result = oneRound();
+  let rockchoice = document.createElement("div");
+  rockchoice.style.width = "100px";
+  rockchoice.style.height = "100px";
+  rockchoice.style.borderRadius = "50%";
+  rockchoice.style.backgroundColor = "transparent";
+  rockchoice.style.border = "solid 20px black";
+  document.querySelector('#game').appendChild(rockchoice);
+  console.log(result);
+});
+paper.addEventListener('click', () => {
+    playerChoice = 1;
+    let result = oneRound();
+    console.log(result);
+  });
+scissors.addEventListener('click', () => {
+    playerChoice = 2;
+    let result = oneRound();
+    console.log(result);
+});
+
